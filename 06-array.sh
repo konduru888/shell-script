fcc688f935ca892fa8d301fb8f4b8261b4fa1279
konduru888 <konduru888@gmail.com>
2023-08-22 23:58:39 +0530

    Hello world script added

---
+++ 06-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#this is called array, instead of single value it can hold multiple values
+PERSONS=("Ramesh" "Suresh" "sachin")
+
+#inside array index always starts with 0
+echo "First Peerson: ${PERSONS[0]}"
