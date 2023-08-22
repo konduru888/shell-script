f2c1d06c45b9a17428276de4e3141831a5fe5594
konduru888 <konduru888@gmail.com>
2023-08-22 23:24:52 +0530

    Hello world script added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hi, Hello world, Iam learning DevOps with AWS"
+PERSON1=$1 #this is the 1st value from commandline
+PERSON2=$2 #this is the second value from commandline
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: iam good, how are you $PERSON1"
