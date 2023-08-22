54a3d0191096f7f1fdcfe4b88f16d38e9b078c05
konduru888 <konduru888@gmail.com>
2023-08-22 23:21:42 +0530

    Hello world script added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-DATE=$()
-echo "Hello, this script is executed at timmestamp: "
+#shell script run the command inside $() and stores the output into variable called DATE.
+DATE=$(date)
+
+echo "Hello, this script is executed at timmestamp: $DATE"
